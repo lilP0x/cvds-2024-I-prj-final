84a22e46489e9e7c68693a2005cf3f1ff25977b3
lilP0x <juanpa20022009@hotmail.com>
2024-05-04 19:10:14 -0500

    Ya la pagina se crea apartir del sql

--- src/main/java/co/edu/eci/cvds/controller/ProductController.java
+++ src/main/java/co/edu/eci/cvds/controller/ProductController.java
@@ -1,3 +1,5 @@
+package co.edu.eci.cvds.controller;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,3 +3,7 @@ INSERT INTO CONFIGURATION (PROPIEDAD, VALOR) VALUES ('1', 'Valor 1');
 
 INSERT INTO PRODUCTO (id, nombre, valor, descuento, descripcionBreve, descripcionTecnica, categoria, impuesto, tipoUsuario, iconUrl) 
 VALUES ('12345', 'Producto A', 100, 10, 'Breve descripción', 'Descripción técnica', 'Categoría 1', 15, 'Usuario tipo 1', 'icono.png');
+
+
+INSERT INTO PRODUCTO (id, nombre, valor, descuento, descripcionBreve, descripcionTecnica, categoria, impuesto, tipoUsuario, iconUrl) 
+VALUES ('12346', 'Producto B', 200, 20, 'Breve descripción B', 'Descripción técnica B', 'Categoría 2', 18, 'Usuario tipo 2', 'icono2.png');
