59f9df94bd77ad2f68dfa72abb89deaf23ad593e
lilP0x <juanpa20022009@hotmail.com>
2024-05-07 11:35:17 -0500

    Ya funciona el cargo de productos con todo y precios

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,8 +2,13 @@ INSERT INTO CONFIGURATION (PROPIEDAD, VALOR) VALUES ('1', 'Valor 1');
 
 
 INSERT INTO PRODUCTO (id, nombre, valor, descuento, descripcionBreve, descripcionTecnica, categoria, impuesto, tipoUsuario, iconUrl) 
-VALUES ('12345', 'Producto A', 100, 10, 'Breve descripción', 'Descripción técnica', 'Categoría 1', 15, 'Usuario tipo 1', 'icono.png');
+VALUES ('12345', 'Producto A', 100000, 10, 'Breve descripción', 'Descripción técnica', 'Categoría 1', 15, 'Usuario tipo 1', 'mechanic.png');
 
 
 INSERT INTO PRODUCTO (id, nombre, valor, descuento, descripcionBreve, descripcionTecnica, categoria, impuesto, tipoUsuario, iconUrl) 
-VALUES ('12346', 'Producto B', 200, 20, 'Breve descripción B', 'Descripción técnica B', 'Categoría 2', 18, 'Usuario tipo 2', 'icono2.png');
+VALUES ('12346', 'Producto B', 200000, 20, 'Breve descripción B', 'Descripción técnica B', 'Categoría 2', 18, 'Usuario tipo 2', 'car-diagnostics.png');
+
+INSERT INTO PRODUCTO (id, nombre, valor, descuento, descripcionBreve, descripcionTecnica, categoria, impuesto, tipoUsuario, iconUrl) 
+VALUES ('12347', 'Producto C', 250000, 15, 'Breve descripción C', 'Descripción técnica C', 'Categoría 3', 12, 'Usuario tipo 3', 'maintenance.png');
+
+
