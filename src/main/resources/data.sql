23e7e56ece30ed2b3883120c57c98468c758efe5
unknown <juan.fernandez-g@labinfo.is.escuelaing.edu.co>
2024-05-23 11:48:34 -0500

    Diosito por favor

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,32 +1,73 @@
 INSERT INTO CONFIGURATION (PROPIEDAD, VALOR) VALUES ('1', 'Valor 1');
 
+INSERT INTO PRODUCTO (id, nombre, valor, descuento, descripcionBreve, descripcionTecnica, categoria, impuesto, tipoUsuario, iconUrl) 
+VALUES ('12345', 'Falla en el motor', 100000, 10, 'No acelera, se queda sin fuerza, No enciende o se apaga repentinamente, Sale humo / vapor, Vibracion o ruido del motor', 'Descripcion tecnica', 'identificar falla', 15, 'Usuario tipo 1', 'car-diagnostics.png');
+
 
 INSERT INTO PRODUCTO (id, nombre, valor, descuento, descripcionBreve, descripcionTecnica, categoria, impuesto, tipoUsuario, iconUrl) 
-VALUES ('12345', 'Falla en el motor', 100000, 10, 'No acelera, se queda sin fuerza ,No enciende o se apaga repentinamente Sale humo / vapor Vibración o ruido del motor', 'Descripcion tecnica', 'indentificar falla', 15, 'Usuario tipo 1', 'mechanic.png');
+VALUES ('123459', 'Falla en los frenos', 50000, 10, 'Frenos largos, Ruido / Chillido en los frenos', 'Descripcion tecnica', 'identificar falla', 15, 'Usuario tipo 1', 'car-diagnostics.png');
+
+-- Inserts adicionales
+INSERT INTO PRODUCTO (id, nombre, valor, descuento, descripcionBreve, descripcionTecnica, categoria, impuesto, tipoUsuario, iconUrl) 
+VALUES ('123461', 'Revision general', 99000, 20, 'Revision visual y preventiva de 87 puntos en los 9 sistemas mas importantes del vehiculo', 'Descripcion tecnica B', 'realizar revision', 18, 'Usuario tipo 2', 'mechanic.png');
 
 INSERT INTO PRODUCTO (id, nombre, valor, descuento, descripcionBreve, descripcionTecnica, categoria, impuesto, tipoUsuario, iconUrl) 
-VALUES ('12346', 'Fall en sistema electrico', 200000, 20, 'Bombillos Vidrios / Ventanas Alarma o radio Retrovisores Tablero Mensajes de falla', 'Descripcion tecnica B', 'indentificar falla', 18, 'Usuario tipo 2', 'car-diagnostics.png');
+VALUES ('1234235672', 'Revision de frenos', 250000, 15, 'Revision de frenos de todo el vehiculo', 'Descripcion tecnica C', 'realizar revision', 12, 'Usuario tipo 3', 'mechanic.png');
 
 INSERT INTO PRODUCTO (id, nombre, valor, descuento, descripcionBreve, descripcionTecnica, categoria, impuesto, tipoUsuario, iconUrl) 
-VALUES ('12347', 'Producto C', 250000, 15, 'Breve descripcion C', 'Descripcion tecnica C', 'Categoria 3', 12, 'Usuario tipo 3', 'maintenance.png');
+VALUES ('12346', 'Fallo en sistema electrico', 200000, 20, 'Bombillos, Vidrios / Ventanas, Alarma o radio, Retrovisores, Tablero, Mensajes de falla', 'Descripcion tecnica B', 'identificar falla', 18, 'Usuario tipo 2', 'car-diagnostics.png');
 
 INSERT INTO PRODUCTO (id, nombre, valor, descuento, descripcionBreve, descripcionTecnica, categoria, impuesto, tipoUsuario, iconUrl) 
-VALUES ('123456', 'Producto A', 100000, 10, 'Breve descripcion', 'Descripcion tecnica', 'Categoria 1', 15, 'Usuario tipo 1', 'mechanic.png');
+VALUES ('123478', 'Falla en la caja de cambio', 79000, 15, 'Al ingresar cambios no acelera o desacelera bien, La palanca de cambios esta dura', 'Descripcion tecnica C', 'identificar falla', 12, 'Usuario tipo 3', 'car-diagnostics.png');
 
 INSERT INTO PRODUCTO (id, nombre, valor, descuento, descripcionBreve, descripcionTecnica, categoria, impuesto, tipoUsuario, iconUrl) 
-VALUES ('123467', 'Producto B', 200000, 20, 'Breve descripcion B', 'Descripcion tecnica B', 'Categoria 2', 18, 'Usuario tipo 2', 'car-diagnostics.png');
+VALUES ('123423453', 'Revision de suspension', 35000, 10, 'Revision de suspension en todo el vehiculo', 'Descripcion tecnica', 'realizar revision', 15, 'Usuario tipo 1', 'mechanic.png');
 
 INSERT INTO PRODUCTO (id, nombre, valor, descuento, descripcionBreve, descripcionTecnica, categoria, impuesto, tipoUsuario, iconUrl) 
-VALUES ('123478', 'Producto C', 250000, 15, 'Breve descripcion C', 'Descripcion tecnica C', 'Categoria 3', 12, 'Usuario tipo 3', 'maintenance.png');
+VALUES ('123452353', 'Revision de aire acondicionado', 35000, 10, 'Revision de aire acondicionado', 'Descripcion tecnica', 'realizar revision', 15, 'Usuario tipo 1', 'mechanic.png');
+
 
 INSERT INTO PRODUCTO (id, nombre, valor, descuento, descripcionBreve, descripcionTecnica, categoria, impuesto, tipoUsuario, iconUrl) 
-VALUES ('123459', 'Producto A', 100000, 10, 'Breve descripcion', 'Descripcion tecnica', 'Categoria 1', 15, 'Usuario tipo 1', 'mechanic.png');
+VALUES ('12347', 'Falla en las llantas y suspension', 79000, 15, 'Golpe fuerte al pasar por un hueco o reductor de velocidad, Ruido en la llanta, Direccion desviada, Vibracion en el volante', 'Descripcion tecnica C', 'identificar falla', 12, 'Usuario tipo 3', 'car-diagnostics.png');
 
 INSERT INTO PRODUCTO (id, nombre, valor, descuento, descripcionBreve, descripcionTecnica, categoria, impuesto, tipoUsuario, iconUrl) 
-VALUES ('123461', 'Producto B', 200000, 20, 'Breve descripcion B', 'Descripcion tecnica B', 'Categoria 2', 18, 'Usuario tipo 2', 'car-diagnostics.png');
+VALUES ('123456', 'Falla en los liquidos / fugas', 55000, 50, 'Encuentras un liquido derramado debajo del carro', 'Descripcion tecnica', 'identificar falla', 15, 'Usuario tipo 1', 'car-diagnostics.png');
 
 INSERT INTO PRODUCTO (id, nombre, valor, descuento, descripcionBreve, descripcionTecnica, categoria, impuesto, tipoUsuario, iconUrl) 
-VALUES ('123472', 'Producto C', 250000, 15, 'Breve descripcion C', 'Descripcion tecnica C', 'Categoria 3', 12, 'Usuario tipo 3', 'maintenance.png');
+VALUES ('123467', 'Falla en los testigos', 200000, 20, 'Diagnostico de testigos encendidos', 'Descripcion tecnica B', 'identificar falla', 18, 'Usuario tipo 2', 'car-diagnostics.png');
+
 
 INSERT INTO PRODUCTO (id, nombre, valor, descuento, descripcionBreve, descripcionTecnica, categoria, impuesto, tipoUsuario, iconUrl) 
-VALUES ('1234563', 'Producto A', 100000, 10, 'Breve descripcion', 'Descripcion tecnica', 'Categoria 1', 15, 'Usuario tipo 1', 'mechanic.png');
+VALUES ('12345867663', 'Revision de aire acondicionado', 35000, 10, 'Revision de aire acondicionado', 'Descripcion tecnica', 'realizar revision', 15, 'Usuario tipo 1', 'mechanic.png');
+
+INSERT INTO PRODUCTO (id, nombre, valor, descuento, descripcionBreve, descripcionTecnica, categoria, impuesto, tipoUsuario, iconUrl) 
+VALUES ('12342675352', 'Cambio de amortiguadores', 286000, 15, 'Servicio de escaner', 'Descripcion tecnica C', 'mantenimientos', 12, 'Usuario tipo 3', 'maintenance.png');
+
+INSERT INTO PRODUCTO (id, nombre, valor, descuento, descripcionBreve, descripcionTecnica, categoria, impuesto, tipoUsuario, iconUrl) 
+VALUES ('12675352', 'Cambio de amortiguadores', 1130000, 15, 'Servicio de Cambio de amortiguadores', 'Descripcion tecnica C', 'mantenimientos', 12, 'Usuario tipo 3', 'maintenance.png');
+
+INSERT INTO PRODUCTO (id, nombre, valor, descuento, descripcionBreve, descripcionTecnica, categoria, impuesto, tipoUsuario, iconUrl) 
+VALUES ('123458696764', 'Servicio de peritaje compra segura',248000, 10, 'El peritaje mas complejo que tenemos. Agrega prueba para medir el nivel de desgaste interno del motor.', 'Descripcion tecnica', 'realizar revision', 15, 'Usuario tipo 1', 'mechanic.png');
+
+INSERT INTO PRODUCTO (id, nombre, valor, descuento, descripcionBreve, descripcionTecnica, categoria, impuesto, tipoUsuario, iconUrl) 
+VALUES ('1234555444', 'Revision tecnomecanica + rg',377500, 10, 'Revision visual y preventiva de 87 puntos en los 9 sistemas mas importantes del vehiculo', 'Descripcion tecnica', 'realizar revision', 15, 'Usuario tipo 1', 'mechanic.png');
+
+INSERT INTO PRODUCTO (id, nombre, valor, descuento, descripcionBreve, descripcionTecnica, categoria, impuesto, tipoUsuario, iconUrl) 
+VALUES ('123427895672', 'Servicio de escaner', 60000, 15, 'Servicio de escaner', 'Descripcion tecnica C', 'realizar revision', 12, 'Usuario tipo 3', 'maintenance.png');
+
+INSERT INTO PRODUCTO (id, nombre, valor, descuento, descripcionBreve, descripcionTecnica, categoria, impuesto, tipoUsuario, iconUrl) 
+VALUES ('123423785672', 'Revision de bateria', 60000, 15, 'Revision de bateria', 'Descripcion tecnica C', 'realizar revision', 12, 'Usuario tipo 3', 'maintenance.png');
+
+INSERT INTO PRODUCTO (id, nombre, valor, descuento, descripcionBreve, descripcionTecnica, categoria, impuesto, tipoUsuario, iconUrl) 
+VALUES ('123446546235672', 'Discos de frenos', 70000, 15, 'Discos de frenos', 'Descripcion tecnica C', 'mantenimientos', 12, 'Usuario tipo 3', 'maintenance.png');
+
+INSERT INTO PRODUCTO (id, nombre, valor, descuento, descripcionBreve, descripcionTecnica, categoria, impuesto, tipoUsuario, iconUrl) 
+VALUES ('1234267535672', 'Cambio de aceite', 70000, 15, 'Servicio de Cambio de aceite', 'Descripcion tecnica C', 'mantenimientos', 12, 'Usuario tipo 3', 'maintenance.png');
+
+
+INSERT INTO PRODUCTO (id, nombre, valor, descuento, descripcionBreve, descripcionTecnica, categoria, impuesto, tipoUsuario, iconUrl) 
+VALUES ('126753554452', 'Alineacion y balanceo', 95000, 15, 'Servicio de Alineacion y balanceo', 'Descripcion tecnica C', 'mantenimientos', 12, 'Usuario tipo 3', 'maintenance.png');
+
+
+INSERT INTO PRODUCTO (id, nombre, valor, descuento, descripcionBreve, descripcionTecnica, categoria, impuesto, tipoUsuario, iconUrl) 
+VALUES ('12675355432523452', 'Latoneria y pintura', 99000, 15, 'Servicio de Pintura', 'Descripcion tecnica C', 'latoneria y pinturas', 12, 'Usuario tipo 3', 'pistola-de-pintura.png');
